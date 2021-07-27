61cea623cf7b0135287f20647bbd13b7682bbe8d
dhairy-online <dhairysrivastava5@gmail.com>
2021-07-27 22:24:42 +0530

    123231

--- commands/suggestion.js
+++ commands/suggestion.js
@@ -10,7 +10,7 @@ module.exports = {
 
         const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
-
+        
         let messageArgs = args.join(' ');
         const embed = new Discord.MessageEmbed()
         .setColor('FADF2E')
